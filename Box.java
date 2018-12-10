bf72fca228c3c9574f2d5b9f85d6c4e33b52ca6b
Daniel Chen <danieljnchen@gmail.com>
2018-12-10 01:35:31 -0500

    Implemented guessing

--- Box.java
+++ Box.java
@@ -1,17 +1,109 @@
 import javafx.scene.control.TextField;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.Stack;
 
 public class Box extends TextField {
-    public static int numSolved = 0;
-    private boolean solved;
-    private Set<Integer> potentialValues;
+    public static Box[][] boxes = new Box[9][9];
+    public static int numSolved;
+    public static boolean changed = false;
+    private static Stack<Guess> guessStack;
+
+    private Stack<Status> statusStack;
+
+    private class Status implements Cloneable {
+        private Set<Integer> potentialValues;
+
+        private Status(Set<Integer> potentialValues) {
+            this.potentialValues = potentialValues;
+        }
+
+        @Override
+        public Status clone() {
+            return new Status((Set<Integer>) ((HashSet<Integer>)potentialValues).clone());
+        }
+    }
+
+    private static class Guess {
+        private Object[] potentialValues;
+        private int index;
+        private int[] cell;
+
+        private Guess(Set<Integer> potentialValues, int[] cell) {
+            this.potentialValues = potentialValues.toArray();
+            this.cell = cell;
+        }
+
+        private boolean hasNext() {
+            return index < potentialValues.length;
+        }
+
+        private Integer next() {
+            return (Integer) potentialValues[index++];
+        }
+    }
 
     public Box() {
         super();
-        potentialValues = new HashSet<>();
+        statusStack = new Stack<>();
+        statusStack.push(new Status(new HashSet<>()));
+        guessStack = new Stack<>();
         clear();
+    }
+
+    public static boolean guess(int[] cell) {
+        if (cell == null) {
+            if (guessStack.isEmpty()) {
+                return false; // grid is unsolvable
+            } else if (!guessStack.peek().hasNext()) {
+                guessStack.pop(); // backtrack to the last layer
+                popAll();
+                return guess(null);
+            } else {
+                popAll();
+                newLayer();
+                boxes[guessStack.peek().cell[0]][guessStack.peek().cell[1]].
+                        setSolved(guessStack.peek().next());
+                return true;
+            }
+        } else {
+            newLayer();
+            guessStack.push(new Guess(boxes[cell[0]][cell[1]].getPotentialValues(), cell));
+            boxes[cell[0]][cell[1]].setSolved(guessStack.peek().next());
+            return true;
+        }
+    }
+
+    /**
+     * Gets the coordinates (r, c) of the box with the least possibilities
+     * @return a pair of coordinates, or null if the min number of possibilities
+     * is 0
+     */
+    public static int[] minPossibilities() {
+        int[] out = new int[2];
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (!boxes[r][c].isSolved()) {
+                    if (out[0] != 0 && out[1] != 0) {
+                        if (boxes[r][c].getPotentialValues().size() <
+                                boxes[out[0]][out[1]].getPotentialValues().size()) {
+                            out[0] = r;
+                            out[1] = c;
+                        }
+                    } else {
+                        out[0] = r;
+                        out[1] = c;
+                    }
+                    if (boxes[out[0]][out[1]].getPotentialValues().size() == 0) {
+                        //System.out.println("Cell (" + r + ", " + c + ") has no options");
+                        return null;
+                    }
+                }
+            }
+        }
+        return out;
     }
 
     public Integer getValue() {
@@ -27,45 +119,96 @@ public class Box extends TextField {
         return null;
     }
 
+    /**
+     * Cross out {@code i} from the list of potential values
+     * @param i the number to cross out
+     * @return -1 if nothing happened, or if there is only one value remaining,
+     * return the value
+     */
     public Integer crossOut(Integer i) {
-        if (!solved) {
-            if (potentialValues.contains(i)) {
-                potentialValues.remove(i);
-                if (potentialValues.size() == 1) {
-                    Integer value = potentialValues.iterator().next();
-                    setSolved(value);
-                    return value;
-                }
+        if (statusStack.peek().potentialValues.contains(i)) {
+            statusStack.peek().potentialValues.remove(i);
+            changed = true;
+            if (statusStack.peek().potentialValues.size() == 1) {
+                Integer value = statusStack.peek().potentialValues.iterator().next();
+                setSolved(value);
+                return value;
             }
         }
         return -1;
     }
 
     public Set<Integer> getPotentialValues() {
-        return potentialValues;
+        return statusStack.peek().potentialValues;
     }
 
     public void setSolved(Integer i) {
-        if (!solved) {
-            setText(String.valueOf(i));
-            solved = true;
-            ++numSolved;
-            setStyle("-fx-background-color: green; -fx-border-color: black;");
+        Iterator<Integer> iterator = getPotentialValues().iterator();
+        while (iterator.hasNext()) {
+            if (!iterator.next().equals(i)) {
+                iterator.remove();
+            }
         }
+        update();
     }
 
     public boolean isSolved() {
-        return solved;
+        return statusStack.peek().potentialValues.size() == 1;
     }
 
     public void clear() {
-        setText("");
-        solved = false;
         numSolved = 0;
-        potentialValues.clear();
         for (int i = 1; i < 10; ++i) {
-            potentialValues.add(i);
+            statusStack.peek().potentialValues.add(i);
         }
-        setStyle("-fx-background-color: white; -fx-border-color: black;");
+        update();
+    }
+
+    public static void clearAll() {
+        for (Box[] r : boxes) {
+            for (Box c : r) {
+                c.clear();
+            }
+        }
+    }
+
+    public static void popAll() {
+        for (Box[] r : boxes) {
+            for (Box c : r) {
+                c.statusStack.pop();
+                c.update();
+            }
+        }
+    }
+
+    public static void newLayer() {
+        for (Box[] r : boxes) {
+            for (Box c : r) {
+                c.statusStack.push(c.statusStack.peek().clone());
+            }
+        }
+    }
+
+    private boolean update() {
+        if (getPotentialValues().size() == 1) {
+            setText(String.valueOf(getPotentialValues().iterator().next()));
+            setStyle("-fx-background-color: green; -fx-border-color: black;");
+            return true;
+        } else {
+            setText("");
+            setStyle("-fx-background-color: white; -fx-border-color: black;");
+            return false;
+        }
+    }
+
+    public static void updateAll() {
+        numSolved = 0;
+        for (Box[] r : boxes) {
+            for (Box c : r) {
+                if (c.update()) {
+                    ++numSolved;
+                }
+            }
+        }
     }
 }
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
@@ -8,29 +9,26 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class SudokuSolver extends Application {
-    private static Box[][] boxes;
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         VBox root = new VBox();
 
-        boxes = new Box[9][9];
-        for (int r = 0; r < boxes.length; ++r) {
-            for (int c = 0; c < boxes[0].length; ++c) {
-                boxes[r][c] = new Box();
+        for (int r = 0; r < Box.boxes.length; ++r) {
+            for (int c = 0; c < Box.boxes[0].length; ++c) {
+                Box.boxes[r][c] = new Box();
             }
         }
 
@@ -38,8 +36,8 @@ public class SudokuSolver extends Application {
         HBox[] rows = new HBox[9];
         for (int r = 0; r < rows.length; ++r) {
             rows[r] = new HBox();
-            for (int c = 0; c < boxes.length; ++c) {
-                rows[r].getChildren().add(boxes[r][c]);
+            for (int c = 0; c < Box.boxes.length; ++c) {
+                rows[r].getChildren().add(Box.boxes[r][c]);
                 if (c % 3 == 2 && c != rows.length - 1) {
                     rows[r].getChildren().add(new Line(0, 0, 0, 25));
                 }
@@ -53,11 +51,24 @@ public class SudokuSolver extends Application {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
 
+        VBox ui = new VBox();
         HBox buttons = new HBox();
         Button solve = new Button("Solve");
-        solve.setOnAction(actionEvent -> (new Solver()).run());
+        solve.setOnAction(actionEvent -> solve());
+        Button guess = new Button("Guess");
+        guess.setOnAction(actionEvent -> Box.guess(Box.minPossibilities()));
         Button clear = new Button("Clear");
-        clear.setOnAction(actionEvent -> clear());
+        clear.setOnAction(actionEvent -> Box.clearAll());
+        Button finish = new Button("Finish");
+        finish.setOnAction(actionEvent -> {
+            while (Box.numSolved < 81) {
+                do {
+                    Box.changed = false;
+                    solve();
+                } while (Box.changed);
+                Box.guess(Box.minPossibilities());
+            }
+        });
         Button chooseFile = new Button("Choose File");
         chooseFile.setOnAction(actionEvent -> {
             File file = fileChooser.showOpenDialog(primaryStage);
@@ -65,42 +76,63 @@ public class SudokuSolver extends Application {
                 loadFromFile(file);
             }
         });
-        buttons.getChildren().addAll(solve, clear, chooseFile);
+        buttons.getChildren().addAll(solve, guess, clear, finish, chooseFile);
+        HBox fileWriting = new HBox();
+        TextField fileName = new TextField();
+        Button writeFile = new Button("Write to File");
+        writeFile.setOnAction(actionEvent -> writeToFile(fileName.getText()));
+        fileWriting.getChildren().addAll(fileName, writeFile);
+        ui.getChildren().addAll(buttons, fileWriting);
 
-        root.getChildren().addAll(rowHolder, buttons);
+        root.getChildren().addAll(rowHolder, ui);
 
         primaryStage.setTitle("Sudoku Solver");
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 300, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public void solve() {
+        class NumFilled {
+            Integer num;
+            boolean filled;
+            public NumFilled(Integer num) {
+                this.num = num;
+            }
+
+            public void setFilled() {
+                filled = true;
+            }
+        }
         // rows
-        for (int r = 0; r < boxes.length; ++r) {
-            Set<Integer> contained = new HashSet<>();
-            for (int c = 0; c < boxes[0].length; ++c) { // find which values are solved
-                Integer i = boxes[r][c].getValue();
+        for (int r = 0; r < Box.boxes.length; ++r) {
+            List<NumFilled> containedList = new ArrayList<>(9);
+            for (int c = 0; c < Box.boxes[0].length; ++c) { // find which values are solved
+                Integer i = Box.boxes[r][c].getValue();
                 if (i != null) {
-                    contained.add(i);
+                    containedList.add(new NumFilled(i));
                 }
             }
-            List<Integer> containedList = new ArrayList<>(9);
-            containedList.addAll(contained);
             for (int i = 0; i < containedList.size(); ++i) {
-                for (int c = 0; c < boxes[0].length; ++c) { // cross out the solved values for all the boxes
-                    Integer solved = boxes[r][c].crossOut(containedList.get(i));
-                    if (solved.intValue() != -1) {
-                        contained.add(solved);
-                        containedList.add(solved);
+                for (int c = 0; c < Box.boxes[0].length; ++c) { // cross out the solved values for all the boxes
+                    if (!containedList.get(i).filled
+                            && Box.boxes[r][c].isSolved()
+                            && Box.boxes[r][c].getValue().equals(
+                            containedList.get(i).num)) {
+                        containedList.get(i).setFilled();
+                    } else {
+                        Integer solved = Box.boxes[r][c].crossOut(containedList.get(i).num);
+                        if (solved != -1) {
+                            containedList.add(new NumFilled(solved));
+                        }
                     }
                 }
             }
             // if only one box has a number that is needed, fill it in
             Map<Integer, Integer> boxesContaining = new HashMap<>();
-            for (int c = 0; c < boxes[0].length; ++c) {
-                if (!boxes[r][c].isSolved()) {
-                    for (Integer i : boxes[r][c].getPotentialValues()) {
+            for (int c = 0; c < Box.boxes[0].length; ++c) {
+                if (!Box.boxes[r][c].isSolved()) {
+                    for (Integer i : Box.boxes[r][c].getPotentialValues()) {
                         if (boxesContaining.containsKey(i)) {
                             if (boxesContaining.get(i).intValue() > 0) {
                                 boxesContaining.put(i, -1);
@@ -114,34 +146,38 @@ public class SudokuSolver extends Application {
             for (Integer i : boxesContaining.keySet()) {
                 int c = boxesContaining.get(i);
                 if (c > 0) {
-                    boxes[r][c].setSolved(i);
+                    Box.boxes[r][c].setSolved(i);
                 }
             }
         }
         // columns
-        for (int c = 0; c < boxes[0].length; ++c) {
-            Set<Integer> contained = new HashSet<>();
-            for (int r = 0; r < boxes.length; ++r) {
-                Integer i = boxes[r][c].getValue();
+        for (int c = 0; c < Box.boxes[0].length; ++c) {
+            List<NumFilled> containedList = new ArrayList<>(9);
+            for (int r = 0; r < Box.boxes.length; ++r) {
+                Integer i = Box.boxes[r][c].getValue();
                 if (i != null) {
-                    contained.add(i);
+                    containedList.add(new NumFilled(i));
                 }
             }
-            List<Integer> containedList = new ArrayList<>(9);
-            containedList.addAll(contained);
             for (int i = 0; i < containedList.size(); ++i) {
-                for (int r = 0; r < boxes.length; ++r) { // cross out the solved values for all the boxes
-                    Integer solved = boxes[r][c].crossOut(containedList.get(i));
-                    if (solved.intValue() != -1) {
-                        contained.add(solved);
-                        containedList.add(solved);
+                for (int r = 0; r < Box.boxes.length; ++r) { // cross out the solved values for all the boxes
+                    if (!containedList.get(i).filled
+                            && Box.boxes[r][c].isSolved()
+                            && Box.boxes[r][c].getValue().equals(
+                            containedList.get(i).num)) {
+                        containedList.get(i).setFilled();
+                    } else {
+                        Integer solved = Box.boxes[r][c].crossOut(containedList.get(i).num);
+                        if (solved.intValue() != -1) {
+                            containedList.add(new NumFilled(solved));
+                        }
                     }
                 }
             }
             Map<Integer, Integer> boxesContaining = new HashMap<>();
-            for (int r = 0; r < boxes[0].length; ++r) {
-                if (!boxes[r][c].isSolved()) {
-                    for (Integer i : boxes[r][c].getPotentialValues()) {
+            for (int r = 0; r < Box.boxes[0].length; ++r) {
+                if (!Box.boxes[r][c].isSolved()) {
+                    for (Integer i : Box.boxes[r][c].getPotentialValues()) {
                         if (boxesContaining.containsKey(i)) {
                             if (boxesContaining.get(i).intValue() > 0) {
                                 boxesContaining.put(i, -1);
@@ -155,31 +191,35 @@ public class SudokuSolver extends Application {
             for (Integer i : boxesContaining.keySet()) {
                 int r = boxesContaining.get(i);
                 if (r > 0) {
-                    boxes[r][c].setSolved(i);
+                    Box.boxes[r][c].setSolved(i);
                 }
             }
         }
 
         // boxes
         for (int b = 0; b < 9; ++b) {
-            Set<Integer> contained = new HashSet<>();
+            List<NumFilled> containedList = new ArrayList<>(9);
             for (int r = (b - (b % 3)); r < b - (b % 3) + 3; ++r) {
                 for (int c = (b % 3) * 3; c < ((b % 3) + 1) * 3; ++c) {
-                    Integer i = boxes[r][c].getValue();
+                    Integer i = Box.boxes[r][c].getValue();
                     if (i != null) {
-                        contained.add(i);
+                        containedList.add(new NumFilled(i));
                     }
                 }
             }
-            List<Integer> containedList = new ArrayList<Integer>(9);
-            containedList.addAll(contained);
             for (int i = 0; i < containedList.size(); ++i) {
                 for (int r = (b - (b % 3)); r < b - (b % 3) + 3; ++r) {
                     for (int c = (b % 3) * 3; c < ((b % 3) + 1) * 3; ++c) {
-                        Integer solved = boxes[r][c].crossOut(containedList.get(i));
-                        if (solved.intValue() != -1) {
-                            contained.add(solved);
-                            containedList.add(solved);
+                        if (!containedList.get(i).filled
+                                && Box.boxes[r][c].isSolved()
+                                && Box.boxes[r][c].getValue().equals(
+                                containedList.get(i).num)) {
+                            containedList.get(i).setFilled();
+                        } else {
+                            Integer solved = Box.boxes[r][c].crossOut(containedList.get(i).num);
+                            if (solved.intValue() != -1) {
+                                containedList.add(new NumFilled(solved));
+                            }
                         }
                     }
                 }
@@ -188,8 +228,8 @@ public class SudokuSolver extends Application {
             Map<Integer, Integer> boxesContaining = new HashMap<>();
             for (int r = (b - (b % 3)); r < b - (b % 3) + 3; ++r) {
                 for (int c = (b % 3) * 3; c < ((b % 3) + 1) * 3; ++c) {
-                    if (!boxes[r][c].isSolved()) {
-                        for (Integer i : boxes[r][c].getPotentialValues()) {
+                    if (!Box.boxes[r][c].isSolved()) {
+                        for (Integer i : Box.boxes[r][c].getPotentialValues()) {
                             if (boxesContaining.containsKey(i)) {
                                 if (boxesContaining.get(i).intValue() > 0) {
                                     boxesContaining.put(i, -1);
@@ -204,31 +244,40 @@ public class SudokuSolver extends Application {
             for (Integer i : boxesContaining.keySet()) {
                 int n = boxesContaining.get(i);
                 if (n > 0) {
-                    boxes[(n - (n % 9)) / 9][n % 9].setSolved(i);
+                    Box.boxes[(n - (n % 9)) / 9][n % 9].setSolved(i);
                 }
             }
         }
+        Box.updateAll();
     }
 
-    private class Solver extends Thread {
-        public void run() {
-            long startTime = System.currentTimeMillis();
-            while (Box.numSolved < 81 && (System.currentTimeMillis() - startTime) < 5000) {
-                solve();
+    public static void writeToFile(String fileName) {
+        File currentDirectory = new File(System.getProperty("user.dir"));
+        if (currentDirectory.listFiles((dir, name) -> {return fileName.equals(name);}).length == 0) {
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+                for (int r = 0; r < 9; ++r) {
+                    for (int c = 0; c < 9; ++c) {
+                        String append = Box.boxes[r][c].getText();
+                        if (append.length() != 0) {
+                            writer.append(Box.boxes[r][c].getText());
+                        }
+                        if (c != 8) {
+                            writer.append(",");
+                        }
+                    }
+                    writer.append(";");
+                }
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("An IO error occurred");
             }
+        } else {
+            System.out.println("File already exists in the current directory; try changing the file name");
         }
     }
-
-    public static void clear() {
-        for (Box[] row : boxes) {
-            for (Box b : row) {
-                b.clear();
-            }
-        }
-    }
-
     public static void loadFromFile(File file) {
-        clear();
+        Box.clearAll();
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
             int c = -1;
@@ -241,7 +290,7 @@ public class SudokuSolver extends Application {
                     ++row;
                     col = 0;
                 } else if ('0' < c && '9' >= c) {
-                    boxes[row][col].setText(String.valueOf(((char) c)));
+                    Box.boxes[row][col].setText(String.valueOf(((char) c)));
                 }
             }
             reader.close();
